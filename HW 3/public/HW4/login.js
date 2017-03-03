0305e9fe849940b1a8605e748b8435ab24d4a7fb
Otto <yuh198@ucsd.edu>
2017-03-03 10:05:33 -0800

    login js

---
+++ HW 3/public/HW4/login.js
@@ -0,0 +1,55 @@
+(function() {
+
+	// Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyA8wmK8-privQ9gc8wiYvsETpYlHLiBGJ8",
+      authDomain: "coffee-dex-a7a8d.firebaseapp.com",
+      databaseURL: "https://coffee-dex-a7a8d.firebaseio.com",
+      storageBucket: "coffee-dex-a7a8d.appspot.com",  
+    };
+    firebase.initializeApp(config);
+
+
+    // Get elements
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+	const btnLogin = document.getElementById('btnLogin');
+	const btnSignup = document.getElementById('btnSignup');
+
+	// Add login event
+	btnLogin.addEventListener('click', e => {
+		// Get email and password
+		const email = txtEmail.value;
+		const password = txtPassword.value;
+		const auth = firebase.auth();
+
+		// Sign in
+		const promise = auth.signInWithEmailAndPassword(email, password);
+		promise.catch(e => console.log(e.message));
+		//window.location.href = "./main_page.html";
+	});
+
+	// Add signup event
+	btnSignup.addEventListener('click', e => {
+		// Get email and password
+		// TODO: check for real email
+		const email = txtEmail.value;
+		const password = txtPassword.value;
+		const auth = firebase.auth();
+
+		// Sign in
+		const promise = auth.createUserWithEmailAndPassword(email, password);
+		promise
+			.catch(e => console.log(e.message));
+	});   
+
+	// Add realtime listener
+	firebase.auth().onAuthStateChanged(firebaseUser => {
+		if (firebaseUser) {
+			console.log(firebaseUser);
+		} else {
+			console.log('not logged in');
+		}
+	}); 
+
+}());
