8758256c9aedab93121e3be61edd20b04c5a44e3
Otto <yuh198@ucsd.edu>
2017-03-04 12:42:01 -0800

    nothing forward

--- HW 3/public/HW4/login.js
+++ HW 3/public/HW4/login.js
@@ -16,6 +16,30 @@
 	const btnLogin = document.getElementById('btnLogin');
 	const btnSignup = document.getElementById('btnSignup');
 
+	var googleProvider = new firebase.auth.GoogleAuthProvider();
+	googleProvider.addScope('https://www.googleapis.com/auth/plus.login');
+
+	googleProvider.setCustomParameters({
+  		'login_hint': 'user@gmail.com'
+	});
+
+	firebase.auth().signInWithPopup(googleProvider).then(function(result) {
+  	// This gives you a Google Access Token. You can use it to access the Google API.
+  	var token = result.credential.accessToken;
+  	// The signed-in user info.
+  	var user = result.user;
+  	// ...
+	}).catch(function(error) {
+  	// Handle Errors here.
+  	var errorCode = error.code;
+  	var errorMessage = error.message;
+  	// The email of the user's account used.
+  	var email = error.email;
+  	// The firebase.auth.AuthCredential type that was used.
+  	var credential = error.credential;
+  	// ...
+	});
+
 	// Add login event
 	btnLogin.addEventListener('click', e => {
 		// Get email and password
@@ -60,5 +84,6 @@
 			console.log('not logged in');
 		}
 	}); 
+
 
 }());
