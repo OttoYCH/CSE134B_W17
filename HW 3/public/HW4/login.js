4a354b3ee5d12153f87d7bae68bdb2106904eb4d
rermert <rermert@ucsd.edu>
2017-03-04 12:41:33 -0800

    Google sign in attempt

--- HW 3/public/HW4/login.js
+++ HW 3/public/HW4/login.js
@@ -1,3 +1,5 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
 (function() {
 
 	// Initialize Firebase
@@ -8,7 +10,6 @@
       storageBucket: "coffee-dex-a7a8d.appspot.com",  
     };
     firebase.initializeApp(config);
-
 
     // Get elements
     const txtEmail = document.getElementById('txtEmail');
@@ -33,7 +34,7 @@
 				window.location.href = "./login.html";
 			}
 		});
-		//promise.then(window.location.href = "./main_page.html", e => console.log(e.message));
+		//promise.then(window.location.hre f = "./main_page.html", e => console.log(e.message));
 		//window.location.href = "./main_page.html";
 	});
 
@@ -62,3 +63,35 @@
 	}); 
 
 }());
+
+function googleSignIn() {
+	console.log("Before");
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+		console.log("During");
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  // ...
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		  // ...
+		});
+		console.log("After");
+}
+
+/*
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+*/
